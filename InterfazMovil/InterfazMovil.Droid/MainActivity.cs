f61e401a808bc224ce78c0c18b6917f160fe9271
github15 <ldmiranda123@gmail.com>
2016-07-05 21:37:11 -0400

    Limpiando el Proyecto Xamarin Forms

--- InterfazMovil/InterfazMovil.Droid/MainActivity.cs
+++ InterfazMovil/InterfazMovil.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace InterfazMovil.Droid
 {
-    [Activity(Label = "Proyecto42", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Proyecto42(Xamarin)", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
